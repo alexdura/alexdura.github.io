ffb807201955e81ca88b9fbf39a2a953920acbdf
Alexandru Dura <alexandru.dura@cs.lth.se>
2025-06-11 15:26:24 +0200

    Add the CV

--- site.hs
+++ site.hs
@@ -14,6 +14,10 @@ main = hakyll $ do
         route   idRoute
         compile copyFileCompiler
 
+    match "files/*" $ do
+      route idRoute
+      compile copyFileCompiler
+
     match "css/*" $ do
         route   idRoute
         compile compressCssCompiler
